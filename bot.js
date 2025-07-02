cf967fa3ceb4d4ae8b618ed583e935deab501534
YuhWu <yuhwu@KILLERWHALE.zoo.lan>
2025-07-02 17:16:33 +0100

    normal gameplay strat

--- bot.js
+++ bot.js
@@ -2,28 +2,25 @@ class Bot { //random
     makeMove(gamestate) {
         rounds = gamestate[rounds];
 
-        pladynamite = 100;
-        oppdynamite = 100;
+        pladynamite, oppdynamite = getdyna(rounds);
 
-        n = rounds.length - 1
-        if (rounds[n - 1]["p1"] == "D") {
-            pladynamite -= 1;
-        } else if (rounds[n - 1]["p2"] == "D") {
-            oppdynamite -= 1;
-        }
-
+        // tie strat
         const tieslen = checkties(gamestate[rounds]);
         if (tieslen > 0){
+            history = extractties(rounds, tieslen); //check single next move across history, and then react
             //analyse their tie strategy from previous ties and react
         }
 
+        // normal gameplay strat
         opprounds = gamestate[rounds].map(round => round[p2]);
         const patternlen = checkpatterns(opprounds);
         if (patternlen != -1) {
-            //react to patterns
+            // pattern
+            return patternreact(opprounds, patternlen);
         }
         else {
-            //react to random
+            // no pattern
+            return randomdynamove();
         }
     }
 }
@@ -53,4 +50,74 @@ function checkties(rounds) {
         }
     }
     return ties;
+}
+
+function patternreact(rounds, patternlen) {
+    pattern = rounds.slice(rounds.length - patternlen);
+    return react(pattern[0]);
+}
+
+function react(move){
+    switch(move) {
+        case "R" : return "P";
+        case "P" : return "S";
+        case "S" : return "R";
+        case "D" : return "W";
+        case "W" : return randomove();
+    }
+}
+
+function randomove(){
+    const rand = Math.random() * 3;
+    switch (Math.floor(rand)) {
+        case 0: 
+            return "R";
+        case 1: 
+            return "P";
+        case 2:
+            return "S";
+    }
+}
+
+function randomdynamove(){
+    const rand = Math.random();
+    dynap = Math.min(0.5, pladynamite/(2500-rounds.length));
+    if (rand < dynap) {
+        return "D";
+    } else {
+        return randomove();
+    }
+}
+
+function getdyna(rounds){
+    pladynamite = 100;
+    oppdynamite = 100;
+
+    for (let i = 0; i < rounds.length; i++) {
+        if (rounds[i]["p1"] == "D") {
+            pladynamite -= 1;
+        }
+        if (rounds[i]["p2"] == "D") {
+            oppdynamite -= 1;
+        }
+    }
+
+    return pladynamite, oppdynamite;
+}
+
+function extractties(rounds, tielen){
+    const result = [];
+    let i = rounds.length-tielen-1;
+    while(i >= 0){
+        let ties = checkties(rounds.slice(0, i+1));
+        if (ties >= tielen) {
+            past = rounds.slice(i - checkties + 1, i+2);
+            result.push(past);
+
+            i -= checkties;
+        } else {
+            i--;
+        }
+    }
+    return result;
 }
